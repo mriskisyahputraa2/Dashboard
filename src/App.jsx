42b8e05569b09adef46c5b233877a3566106fac8
mriskisyahputraa2 <riskideveloper2@gmail.com>
2023-05-31 15:19:02 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -10,29 +10,14 @@ function App() {
     <>
       <div>
         <BrowserRouter>
-          <div className="relative dark:bg-main-dark-bg">
+          <div className="flex relative dark:bg-main-dark-bg">
             <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
-              {/* icons setting */}
-              <TooltipComponent content="Settings" position="Top">
-                <button
-                  type="button"
-                  className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white"
-                  style={{ background: "blue", borderRadius: "50%" }}>
+              <TooltipComponent>
+                <button>
                   <FiSettings />
                 </button>
               </TooltipComponent>
             </div>
-            {activeMenu ? (
-              <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
-                Sidebar
-              </div>
-            ) : (
-              <div className="w-0 dark:bg-secondary-dark-bg">Sidebar w-0</div>
-            )}
-            <div
-              className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${
-                activeMenu ? "md:ml-72" : "flex-2"
-              } `}></div>
           </div>
         </BrowserRouter>
       </div>
